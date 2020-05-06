a93fbb0e7ed4d2efd4a144f9a01d5e38556a8ab4
Larry Jones <larry.jones@reveal-energy.com>
2020-05-06 18:59:54 -0500

    Prepare for writing next tests

--- tests/test_project_adapter.py
+++ tests/test_project_adapter.py
@@ -40,20 +40,24 @@ class TestProjectLoader(unittest.TestCase):
         patched_loader = image_frac.ProjectLoader('dont_care')
         patched_loader.loaded_project = unittest.mock.MagicMock(name='stub_project')
         # We do not need an actual item, only an array with the correct number of items: 1
-        stub_wells = StubWells(['dont_care_item'])
+        dont_care_items = ['dont_care_item']
+        stub_wells = StubWells(dont_care_items)
         patched_loader.loaded_project.return_value.Wells = stub_wells
-        expected_well_id = uuid.UUID('cbc82ce5-f8f4-400e-94fc-03a95635f18b')
-        stub_uuid_module.uuid4.return_value = expected_well_id
+        uuid_strings = ['cbc82ce5-f8f4-400e-94fc-03a95635f18b']
+        expected_well_ids = [uuid.UUID(s) for s in uuid_strings]
+        stub_uuid_module.uuid4.side_effect = expected_well_ids
         sut = image_frac.ProjectAdapter(patched_loader)
         # noinspection PyTypeChecker
-        assert_that(sut.well_ids(), contains_exactly(expected_well_id))
+        # Unpack `expected_well_ids` because `contains_exactly` expects multiple items
+        assert_that(sut.well_ids(), contains_exactly(*expected_well_ids))
 
     @unittest.mock.patch('image_frac.project_adapter.uuid', name='stub_uuid_module', autospec=True)
     def test_many_wells_ids_for_project_with_many_wells(self, stub_uuid_module):
         patched_loader = image_frac.ProjectLoader('dont_care')
         patched_loader.loaded_project = unittest.mock.MagicMock(name='stub_project')
         # We do not need an actual item, only an array with the correct number of items: 1
-        stub_wells = StubWells(['dont_care_1', 'dont_care_2', 'dont_care_3'])
+        dont_care_items = ['dont_care_1', 'dont_care_2', 'dont_care_3']
+        stub_wells = StubWells(dont_care_items)
         patched_loader.loaded_project.return_value.Wells = stub_wells
         uuid_strings = ['0b09aae5-8355-4968-815c-5622dfc7aac6',
                         'a1ba308d-c3d9-4314-bc21-d6bbb80ebcf8', 'cbde9d6f-2c95-4d8b-a1b8-5235194d0fa6']
