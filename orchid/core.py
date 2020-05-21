9c64ca231c02a2f3453e754e10cd5af362071835
Larry Jones <larry.jones@reveal-energy.com>
2020-05-21 13:33:36 -0500

    Change pressure plot colors to well default colors
    Previous commits plotted all pressure curves in the same color.
    
    This commit changes those colors to use colors from the project default
    well colors.
    
    I suspect we may need to do additional work so that the trajectory and
    the pressure curve are plotted, by default in the same color.

--- orchid/core.py
+++ orchid/core.py
@@ -47,7 +47,6 @@ def load_project(ifrac_pathname: str) -> ProjectAdapter:
 # might actually be right now so I have not included the argument. Adding this argument is low-cost.
 #
 # Candidate fixes:
-# - Use well-default colors
 # - Extract plot single onto subplot axes
 def plot_pressures(ifrac_pathname: str) -> None:
     """
@@ -57,30 +56,30 @@ def plot_pressures(ifrac_pathname: str) -> None:
     :return: None
     """
     project = load_project(ifrac_pathname)
+    wells_facade = project.all_wells()
+    default_well_colors = ['#%02x%02x%02x' % (r, g, b) for (r, g, b) in wells_facade.default_well_colors()]
     all_pressure_curves = project.all_pressure_curves()
     pressure_curve_ids = all_pressure_curves.pressure_curve_ids()
     pressure_curve_display_names = [all_pressure_curves.display_name(curve_id) for curve_id in pressure_curve_ids]
     surface_pressure_curves = [all_pressure_curves.pressure_curve_samples(pressure_curve_id)
                                for pressure_curve_id in pressure_curve_ids]
+
     # TODO: Remove hard-coding
     figure, axes = plt.subplots(2, 2)
     curves_to_plot = np.reshape(surface_pressure_curves, (2, 2))
     names_to_display = np.reshape(pressure_curve_display_names, (2, 2))
+    # TODO: Do we need a better way to map colors to curves?
+    # The following code assumes that the colors on the trajectories for each well will be identical to the
+    # colors for the curves. I do not know of any guarantee that the order of curves in the time series is
+    # the same order as the pressure curves. :(
+    colors_to_use = np.reshape(default_well_colors[:4], (2, 2))
     for i in range(len(axes)):
         for j in range(len(axes[0])):
-            curves_to_plot[i, j].plot(ax=axes[i, j])
+            curves_to_plot[i, j].plot(ax=axes[i, j], color=colors_to_use[i, j])
             axes[i, j].set_ylabel(f'Pressure ({project.pressure_unit()})')
             axes[i, j].title.set_text(names_to_display[i, j])
             x_tick_labels = axes[i, j].get_xticklabels()
             plt.setp(x_tick_labels, rotation=30)
-    # for i in range(len(pressure_curve_ids)):
-    #     plt.plot([p.x for p in trajectories[i]], [p.y for p in trajectories[i]],
-    #              label=f'{all_wells.well_display_name(well_ids[i])}',
-    #              color=default_well_colors[i % len(default_well_colors)])
-    # plt.title(f'{project.name()} Well Trajectories (Project Coordinates)')
-    # plt.legend(loc='best')
-    # plt.xlabel(f'Easting ({project.length_unit()})')
-    # plt.ylabel(f'Northing ({project.length_unit()})')
 
     plt.show()
 
