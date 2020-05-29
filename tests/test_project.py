aaae685613e541dc11d0db7f740b31f09f877345
Larry Jones <larry.jones@reveal-energy.com>
2020-05-29 02:11:09 -0500

    Move project units unit tests to separate module

--- tests/test_project.py
+++ tests/test_project.py
@@ -20,7 +20,6 @@ from hamcrest import assert_that, equal_to, instance_of, calling, raises
 from orchid.project_monitor_pressure_curves import ProjectMonitorPressureCurves
 from orchid.project import ProjectAdapter
 from orchid.project_loader import ProjectLoader
-import orchid.project_units as project_units
 from orchid.project_wells import ProjectWells
 from tests.stub_net import create_stub_net_project
 
@@ -76,48 +75,6 @@ class TestProject(unittest.TestCase):
         sut = create_sut(stub_net_project)
 
         assert_that(sut.all_wells(), instance_of(ProjectWells))
-
-    def test_returns_meter_project_unit_from_net_project_length_units(self):
-        stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                   project_length_unit_abbreviation='m')
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('length'), equal_to('m'))
-
-    def test_returns_feet_project_unit_from_net_project_length_units(self):
-        stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                   project_length_unit_abbreviation='ft')
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('length'), equal_to('ft'))
-
-    def test_returns_kPa_project_unit_from_net_project_pressure_units(self):
-        stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                   project_pressure_unit_abbreviation='kPa')
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('pressure'), equal_to('kPa'))
-
-    def test_returns_psi_project_unit_from_net_project_pressure_units(self):
-        stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                   project_pressure_unit_abbreviation='psi')
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('pressure'), equal_to('psi'))
-
-    def test_returns_bpm_project_unit_from_net_slurry_rate_units(self):
-        stub_net_project = create_stub_net_project(slurry_rate_unit_abbreviation='bbl/min',
-                                                   well_names=['dont-care-well'])
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('slurry rate'), equal_to('bbl/min'))
-
-    def test_returns_lb_per_gal_project_unit_from_net_proppant_concentration_units(self):
-        stub_net_project = create_stub_net_project(
-            proppant_concentration_unit_abbreviation='lb/gal (U.S.)', well_names=['dont-care-well'])
-        sut = create_sut(stub_net_project)
-
-        assert_that(sut.unit('proppant concentration'), equal_to('lb/gal (U.S.)'))
 
 
 def create_sut(stub_net_project):
