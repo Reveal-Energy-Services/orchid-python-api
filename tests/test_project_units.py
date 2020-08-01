d979e7e4ae092c2cc1b259518950cfe3a256379d
Larry Jones <larry.jones@reveal-energy.com>
2020-08-01 16:05:25 -0500

    Eliminate common test setup code

--- tests/test_project_units.py
+++ tests/test_project_units.py
@@ -25,39 +25,21 @@ from tests.stub_net import create_stub_net_project
 class TestProjectUnits(unittest.TestCase):
 
     def test_returns_correct_unit_for_project(self):
-        about_units = [('length', 'project_length_unit_abbreviation', ('ft', 'm')),
-                       ('pressure', 'project_pressure_unit_abbreviation', ('psi', 'kPa'))]
+        about_units = [('length', 'project_length_unit_abbreviation', ('ft', 'm'), ('ft', 'm')),
+                       ('pressure', 'project_pressure_unit_abbreviation', ('psi', 'kPa'), ('psi', 'kPa')),
+                       ('slurry rate', 'slurry_rate_unit_abbreviation',
+                        ('bbl/min', 'm^3/min'), ('bbl/min', 'm\u00b3/min')),
+                       ('proppant concentration', 'proppant_concentration_unit_abbreviation',
+                        ('lb/gal (U.S.)', 'kg/m^3'), ('lb/gal (U.S.)', 'kg/m\u00b3'))]
         default_options = {'well_names': ['dont-care-well']}
-        for quantity, abbreviation_name, units in about_units:
-            for unit in units:
+        for quantity, abbreviation_name, abbreviations, units in about_units:
+            for abbreviation, unit in zip(abbreviations, units):
                 with self.subTest(unit=unit):
-                    options = toolz.assoc(default_options, abbreviation_name, unit)
+                    options = toolz.assoc(default_options, abbreviation_name, abbreviation)
                     stub_net_project = create_stub_net_project(**options)
                     sut = create_sut(stub_net_project)
 
                     assert_that(sut.unit(quantity), equal_to(unit))
-
-    def test_returns_correct_slurry_rate_unit_for_project(self):
-        slurry_rate_abbreviations = ['bbl/min', 'm^3/min']
-        units = ['bbl/min', 'm\u00b3/min']
-        for (abbreviation, unit) in zip(slurry_rate_abbreviations, units):
-            with self.subTest(abbreviation=abbreviation, unit=unit):
-                stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                           slurry_rate_unit_abbreviation=abbreviation)
-                sut = create_sut(stub_net_project)
-
-                assert_that(sut.unit('slurry rate'), equal_to(unit))
-
-    def test_returns_correct_proppant_concentration_unit_for_project(self):
-        proppant_concentration_abbreviations = ['lb/gal (U.S.)', 'kg/m^3']
-        units = ['lb/gal (U.S.)', 'kg/m\u00b3']
-        for (abbreviation, unit) in zip(proppant_concentration_abbreviations, units):
-            with self.subTest(abbreviation=abbreviation, unit=unit):
-                stub_net_project = create_stub_net_project(well_names=['dont-care-well'],
-                                                           proppant_concentration_unit_abbreviation=abbreviation)
-                sut = create_sut(stub_net_project)
-
-                assert_that(sut.unit('proppant concentration'), equal_to(unit))
 
 
 def create_sut(stub_net_project):
