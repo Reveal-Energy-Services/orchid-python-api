ff60c63d3766e776a5a820de601db496d91cc710
Larry Jones <larry.jones@reveal-energy.com>
2020-08-09 01:21:53 -0500

    Update copyright to support Apache 2.0 license

--- plot_trajectories.py
+++ plot_trajectories.py
@@ -1,16 +1,19 @@
-#! py -3
+#! /usr/bin/env python
+#  Copyright 2017-2020 Reveal Energy Services, Inc 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); 
+#  you may not use this file except in compliance with the License. 
+#  You may obtain a copy of the License at 
+#
+#      http://www.apache.org/licenses/LICENSE-2.0 
+#
+#  Unless required by applicable law or agreed to in writing, software 
+#  distributed under the License is distributed on an "AS IS" BASIS, 
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+#  See the License for the specific language governing permissions and 
+#  limitations under the License. 
 #
 # This file is part of Orchid and related technologies.
-#
-# Copyright (c) 2017-2020 Reveal Energy Services.  All Rights Reserved.
-#
-# LEGAL NOTICE:
-# Orchid contains trade secrets and otherwise confidential information
-# owned by Reveal Energy Services. Access to and use of this information is 
-# strictly limited and controlled by the Company. This file may not be copied,
-# distributed, or otherwise disclosed outside of the Company's facilities 
-# except under appropriate precautions to maintain the confidentiality hereof, 
-# and may not be used in any way not expressly authorized by the Company.
 #
 
 import argparse
