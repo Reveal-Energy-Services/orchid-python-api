a926ad9cd42524ff1dad716053b649c5080f146d
Larry Jones <larry.jones@reveal-energy.com>
2020-08-14 11:59:13 -0500

    Remove extraneous "project" from test names
    Previous commits had names like `test_project_name`.
    
    This commit simplifies those names to, for example, `test_name`.

--- tests/test_project.py
+++ tests/test_project.py
@@ -29,46 +29,46 @@ class TestProject(unittest.TestCase):
     def test_canary(self):
         assert_that(2 + 2, equal_to(4))
 
-    def test_project_name(self):
+    def test_name(self):
         stub_native_project = create_stub_net_project(name='commodorum')
         sut = create_sut(stub_native_project)
         assert_that(sut.name, equal_to('commodorum'))
 
-    def test_project_wells_if_no_wells(self):
+    def test_wells_if_no_wells(self):
         stub_native_project = create_stub_net_project(name='exsistet')
         sut = create_sut(stub_native_project)
         assert_that(sut.wells, contains_exactly())
 
-    def test_project_wells_if_one_well(self):
+    def test_wells_if_one_well(self):
         expected_well_names = ['clunibus']
         stub_native_project = create_stub_net_project(name='exsistet', well_names=expected_well_names)
         sut = create_sut(stub_native_project)
         assert_that(map(lambda w: w.name, sut.wells), contains_exactly(*expected_well_names))
 
-    def test_project_wells_if_many_wells(self):
+    def test_wells_if_many_wells(self):
         expected_well_names = ['cordam', 'turbibus', 'collaris']
         stub_native_project = create_stub_net_project(name='exsistet', well_names=expected_well_names)
         sut = create_sut(stub_native_project)
         assert_that(map(lambda w: w.name, sut.wells), contains_exactly(*expected_well_names))
 
-    def test_project_wells_by_name_if_no_wells(self):
+    def test_wells_by_name_if_no_wells(self):
         stub_native_project = create_stub_net_project(name='exsistet')
         sut = create_sut(stub_native_project)
         assert_that(sut.wells_by_name('clunibus'), contains_exactly())
 
-    def test_project_wells_by_name_if_no_well_with_name_found(self):
+    def test_wells_by_name_if_no_well_with_name_found(self):
         expected_well_names = ['clunibus']
         stub_native_project = create_stub_net_project(name='exsistet', well_names=expected_well_names)
         sut = create_sut(stub_native_project)
         assert_that(map(lambda w: w.name, sut.wells_by_name('clunibus')), contains_exactly(*expected_well_names))
 
-    def test_project_wells_by_name_if_one_well_with_name_found(self):
+    def test_wells_by_name_if_one_well_with_name_found(self):
         expected_well_names = ['clunibus']
         stub_native_project = create_stub_net_project(name='exsistet', well_names=['clunibus'])
         sut = create_sut(stub_native_project)
         assert_that(map(lambda w: w.name, sut.wells_by_name('clunibus')), contains_exactly(*expected_well_names))
 
-    def test_project_wells_by_name_if_many_wells_with_name_found(self):
+    def test_wells_by_name_if_many_wells_with_name_found(self):
         stub_native_project = create_stub_net_project(name='exsistet',
                                                       well_names=['cordam', 'turbibus',
                                                                   'cordam', 'collaris',
@@ -84,13 +84,13 @@ class TestProject(unittest.TestCase):
         sut = create_sut(stub_native_project)
         assert_that(sut.default_well_colors(), equal_to([tuple([])]))
 
-    def test_project_default_well_colors_if_one_default_well_color(self):
+    def test_default_well_colors_if_one_default_well_color(self):
         stub_native_project = create_stub_net_project(name='exsistet', default_well_colors=[[0.142, 0.868, 0.220]])
         sut = create_sut(stub_native_project)
         # noinspection PyTypeChecker
         assert_that(sut.default_well_colors(), contains_exactly((0.142, 0.868, 0.220)))
 
-    def test_project_default_well_colors_if_many_default_well_colors(self):
+    def test_default_well_colors_if_many_default_well_colors(self):
         expected_default_well_colors = [(0.610, 0.779, 0.675), (0.758, 0.982, 0.720), (0.297, 0.763, 0.388)]
         stub_native_project = create_stub_net_project(name='exsistet',
                                                       default_well_colors=[list(t) for t
