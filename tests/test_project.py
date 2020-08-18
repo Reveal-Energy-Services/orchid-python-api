d7b7985ecf8a8eb18022fbe3b1608a4130f37c3d
Larry Jones <larry.jones@reveal-energy.com>
2020-08-18 02:55:18 -0500

    Simplify code
    Previous code used a nested list comprehension to calculate the curve
    samples.
    
    This commit replaces that nested list comprehension with a pipeline.

--- tests/test_project.py
+++ tests/test_project.py
@@ -113,13 +113,20 @@ class TestProject(unittest.TestCase):
         # noinspection PyTypeChecker
         assert_that(sut.well_time_series(), is_(empty()))
 
+    @staticmethod
+    @toolz.curry
+    def make_samples_at_index(start, index_value_pair):
+        n, value = index_value_pair
+        return start + datetime.timedelta(seconds=30) * n, value
+
     def test_well_time_series_returns_one_if_one_well_time_series(self):
         curve_name = 'gestum'
         curve_quantity = 'pressure'
         sample_start = datetime.datetime(2018, 11, 14, 0, 58, 32, 136000)
         sample_values = [0.617, 0.408, 2.806]
-        samples = [(sample_start + datetime.timedelta(seconds=30) * i, sample_values[i])
-                   for i in range(len(sample_values))]
+
+        samples = toolz.pipe(enumerate(sample_values),
+                             toolz.map(self.make_samples_at_index(sample_start)))
         stub_native_project = create_stub_net_project(name='non curo', curve_names=[curve_name],
                                                       curves_physical_quantities=[curve_quantity],
                                                       samples=[samples])
@@ -134,9 +141,17 @@ class TestProject(unittest.TestCase):
         sample_starts = [datetime.datetime(2019, 3, 7, 10, 2, 13, 131000),
                          datetime.datetime(2019, 8, 1, 16, 50, 45, 500000),
                          datetime.datetime(2016, 3, 21, 20, 15, 19, 54000)]
-        sample_values = [[152.4, 155.3, 142.0], [246.6, 219.4, 213.0], [219.9, 191.5, 187.6]]
-        samples = [[(sample_start + datetime.timedelta(seconds=30) * i, sample_values[i])
-                   for i in range(len(sample_values))] for sample_start in sample_starts]
+        samples_values = [[152.4, 155.3, 142.0], [246.6, 219.4, 213.0], [219.9, 191.5, 187.6]]
+
+        @toolz.curry
+        def make_samples(start, values):
+            return list(toolz.map(self.make_samples_at_index(start), enumerate(values)))
+
+        samples = list(toolz.pipe(zip(sample_starts, samples_values),
+                                  toolz.map(lambda start_values_pair: make_samples(start_values_pair[0],
+                                                                                   start_values_pair[1]))))
+        # samples = [[(sample_start + datetime.timedelta(seconds=30) * i, samples_values[i])
+        #            for i in range(len(samples_values))] for sample_start in sample_starts]
         stub_native_project = create_stub_net_project(name='non curo', curve_names=curve_names,
                                                       curves_physical_quantities=curve_quantity_names,
                                                       samples=samples)
