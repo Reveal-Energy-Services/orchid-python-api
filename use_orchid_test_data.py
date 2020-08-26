fdc9aa7f941f4596824ec36b853aabd73aab67e6
Larry Jones <larry.jones@reveal-energy.com>
2020-08-26 16:51:18 -0500

    A BROKEN implementation for replacement

--- use_orchid_test_data.py
+++ use_orchid_test_data.py
@@ -15,8 +15,47 @@
 
 
 import argparse
+import glob
+import pathlib
+import re
+import shutil
 import sys
+import tempfile
 from typing import Optional, Sequence
+
+
+def sed_inplace(filename, pattern: str, repl: str):
+    """
+    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+
+    This implementation was copied from [StackOverflow](
+    https://stackoverflow.com/questions/4427542/how-to-do-sed-like-text-replace-with-python/31499114)
+    retrieved on 26-Aug-2020.
+    """
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+    # writing with updating). This is usually a good thing. In this case,
+    # however, binary writing imposes non-trivial encoding constraints trivially
+    # resolved by switching to text writing. Let's do that.
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                print()
+                print(f'Transforming: "{filename}"')
+                transformed_line = pattern_compiled.sub(repl, line)
+                if transformed_line != line:
+                    # `end=''` removes automatic newline
+                    print(f'  Before: "{line}"', end='')
+                    print(f'  After:  "{transformed_line}"', end='')
+                tmp_file.write(pattern_compiled.sub(repl, line))
+
+    # Overwrite the original file with the munged temporary file in a
+    # manner preserving file attributes (e.g., permissions).
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, filename)
 
 
 def change_examples_test_dir(test_data_dir: str, examples_dir: str) -> None:
@@ -27,7 +66,9 @@ def change_examples_test_dir(test_data_dir: str, examples_dir: str) -> None:
         test_data_dir: The directory containing the installed Orchid test data.
         examples_dir: The directory containing the Orchid Python API examples.
     """
-    print(f'Change directory of examples in "{examples_dir}" to "{test_data_dir}".')
+    for example_path in glob.glob(str(pathlib.Path(examples_dir).joinpath('*.ipynb'))):
+        sed_inplace(example_path, r'\path\to', test_data_dir)
+        print(f'Change test data directory(ies) of "{example_path}" to "{test_data_dir}".')
 
 
 def main(cli_args: Optional[Sequence[str]] = None):
