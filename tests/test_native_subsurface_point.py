88af7845f91d2591e1fde05dd52ec1b261461d64
Larry Jones <larry.jones@reveal-energy.com>
2020-09-25 08:12:58 -0500

    Eliminate common test set up code

--- tests/test_native_subsurface_point.py
+++ tests/test_native_subsurface_point.py
@@ -12,13 +12,11 @@
 # and may not be used in any way not expressly authorized by the Company.
 #
 
-import math
+from collections import namedtuple
 import unittest
 import unittest.mock
 
 from hamcrest import assert_that, equal_to, close_to
-from hamcrest.core.base_matcher import BaseMatcher, T
-from hamcrest.core.description import Description
 
 import orchid.measurement as om
 import orchid.native_subsurface_point as nsp
@@ -31,80 +29,85 @@ from Orchid.FractureDiagnostics import ISubsurfacePoint
 import UnitsNet
 
 
+ScalarQuantity = namedtuple('ScalarQuantity', ['magnitude', 'unit'])
+
+
+def create_sut(x=None, y=None, depth=None, md_kelly_bushing=None, xy_origin=None, depth_origin=None):
+    stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
+    if x:
+        actual_x = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(x.magnitude),
+                                        x.unit.net_unit)
+        stub_subsurface_point.X = actual_x
+    if y:
+        actual_y = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(y.magnitude),
+                                        y.unit.net_unit)
+        stub_subsurface_point.Y = actual_y
+    if depth:
+        actual_depth = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(depth.magnitude),
+                                            depth.unit.net_unit)
+        stub_subsurface_point.Depth = actual_depth
+    if md_kelly_bushing:
+        actual_md_kelly_bushing = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(md_kelly_bushing.magnitude),
+                                                       md_kelly_bushing.unit.net_unit)
+        stub_subsurface_point.MdKellyBushing = actual_md_kelly_bushing
+    if xy_origin:
+        stub_subsurface_point.WellReferenceFrameXy = xy_origin
+    if depth_origin:
+        stub_subsurface_point.DepthDatum = depth_origin
+
+    sut = nsp.SubsurfacePoint(stub_subsurface_point)
+    return sut
+
+
 # Test ideas
 class TestNativeSubsurfacePoint(unittest.TestCase):
     def test_canary(self):
         self.assertEqual(2 + 2, 4)
 
     def test_x(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
-        x_magnitude = -2725.83
-        actual_x = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(x_magnitude),
-                                        units.Metric.LENGTH.net_unit)
-        stub_subsurface_point.X = actual_x
+        scalar_x = ScalarQuantity(-2725.83, units.Metric.LENGTH)
+        sut = create_sut(x=scalar_x)
 
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
-
-        expected_x = om.make_measurement(x_magnitude, units.Metric.LENGTH.abbreviation)
+        expected_x = om.make_measurement(scalar_x.magnitude, scalar_x.unit.abbreviation)
         assert_that(sut.x.unit, equal_to(expected_x.unit))
         assert_that(sut.x.magnitude, close_to(expected_x.magnitude, 6e-2))
 
     def test_y(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
-        y_magnitude = 1656448.10
-        actual_y = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(y_magnitude),
-                                        units.Metric.LENGTH.net_unit)
-        stub_subsurface_point.Y = actual_y
+        scalar_y = ScalarQuantity(1656448.10, units.Metric.LENGTH)
+        sut = create_sut(y=scalar_y)
 
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
-
-        expected_y = om.make_measurement(y_magnitude, units.Metric.LENGTH.abbreviation)
+        expected_y = om.make_measurement(scalar_y.magnitude, scalar_y.unit.abbreviation)
         assert_that(sut.y.unit, equal_to(expected_y.unit))
         assert_that(sut.y.magnitude, close_to(expected_y.magnitude, 6e-2))
 
     def test_depth(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
-        depth_magnitude = 1656448.10
-        actual_depth = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(depth_magnitude),
-                                            units.Metric.LENGTH.net_unit)
-        stub_subsurface_point.Depth = actual_depth
+        scalar_depth = ScalarQuantity(8945.60, units.UsOilfield.LENGTH)
+        sut = create_sut(depth=scalar_depth)
 
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
-
-        expected_depth = om.make_measurement(depth_magnitude, units.Metric.LENGTH.abbreviation)
+        expected_depth = om.make_measurement(scalar_depth.magnitude, scalar_depth.unit.abbreviation)
         assert_that(sut.depth.unit, equal_to(expected_depth.unit))
         assert_that(sut.depth.magnitude, close_to(expected_depth.magnitude, 6e-2))
 
     def test_md_kelly_bushing(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
-        md_kelly_bushing_magnitude = 1656448.10
-        actual_md_kelly_bushing = UnitsNet.Length.From(UnitsNet.QuantityValue.op_Implicit(md_kelly_bushing_magnitude),
-                                                       units.Metric.LENGTH.net_unit)
-        stub_subsurface_point.MdKellyBushing = actual_md_kelly_bushing
+        scalar_md_kelly_bushing = ScalarQuantity(3131.45, units.Metric.LENGTH)
+        sut = create_sut(md_kelly_bushing=scalar_md_kelly_bushing)
 
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
-
-        expected_md_kelly_bushing = om.make_measurement(md_kelly_bushing_magnitude, units.Metric.LENGTH.abbreviation)
+        expected_md_kelly_bushing = om.make_measurement(scalar_md_kelly_bushing.magnitude,
+                                                        units.Metric.LENGTH.abbreviation)
         assert_that(sut.md_kelly_bushing.unit, equal_to(expected_md_kelly_bushing.unit))
         assert_that(sut.md_kelly_bushing.magnitude, close_to(expected_md_kelly_bushing.magnitude, 6e-2))
 
     def test_xy_origin(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
         expected_xy_origin = oro.WellReferenceFrameXy.PROJECT
-        stub_subsurface_point.WellReferenceFrameXy = expected_xy_origin
-
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
+        sut = create_sut(xy_origin=expected_xy_origin)
 
         # The expected Python Enum equals the .NET Enum because the expected value of the Python Enum **is**
         # the .NET Enum.
         assert_that(sut.xy_origin, equal_to(expected_xy_origin))
 
     def test_depth_origin(self):
-        stub_subsurface_point = unittest.mock.MagicMock(name='stub_subsurface_point', spec=ISubsurfacePoint)
         expected_depth_origin = oro.DepthDatum.GROUND_LEVEL
-        stub_subsurface_point.DepthDatum = expected_depth_origin
-
-        sut = nsp.SubsurfacePoint(stub_subsurface_point)
+        sut = create_sut(depth_origin=expected_depth_origin)
 
         # The expected Python Enum equals the .NET Enum because the expected value of the Python Enum **is**
         # the .NET Enum.
