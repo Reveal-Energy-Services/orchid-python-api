e9ef00a940090e5ce39657898218162634962e0f
Larry Jones <larry.jones@reveal-energy.com>
2020-10-24 11:35:17 -0500

    Reduce common code

--- tests/test_native_treatment_calculations.py
+++ tests/test_native_treatment_calculations.py
@@ -30,6 +30,9 @@ from Orchid.FractureDiagnostics.Calculations import ITreatmentCalculations, IFra
 import UnitsNet
 
 
+DONT_CARE_MEASUREMENT_MAGNITUDE = float('NaN')
+
+
 class TestNativeTreatmentCalculationsAdapter(unittest.TestCase):
     def test_canary(self):
         assert_that(2 + 2, equal_to(4))
@@ -49,23 +52,13 @@ class TestNativeTreatmentCalculationsAdapter(unittest.TestCase):
                                                            tolerance=6e-3)
 
     def test_median_treating_pressure_returns_get_median_treating_pressure_warnings(self):
-        stub_native_treatment_calculations = unittest.mock.MagicMock(name='stub_calculations',
-                                                                     autospec=ITreatmentCalculations)
-        stub_native_calculations_factory = unittest.mock.MagicMock(name='stub_calculations_factory',
-                                                                   autospec=IFractureDiagnosticsCalculationsFactory)
-        stub_native_calculations_factory.TreatmentCalculations = unittest.mock.MagicMock(
-            return_value=stub_native_treatment_calculations)
-
         stub_stage = unittest.mock.Mock('stub_stage_adapter', autospec=nsa.NativeStageAdapter)
         start_time = datetime.datetime(2023, 7, 2, 3, 57, 19)
         stop_time = datetime.datetime(2023, 7, 2, 5, 30, 2)
         for expected_warnings in [[], ['lente vetustas lupam vicit'], ['clunis', 'nobile', 'complacuit']]:
-            stub_native_calculation_result = unittest.mock.MagicMock(name='stub_calculation_result')
-            stub_native_calculation_result.Result = UnitsNet.Pressure.From(
-                UnitsNet.QuantityValue.op_Implicit(61.95835749857516), units.Metric.PRESSURE.net_unit)
-            stub_native_calculation_result.Warnings = expected_warnings
-            stub_native_treatment_calculations.GetMedianTreatmentPressure = unittest.mock.MagicMock(
-                return_value=stub_native_calculation_result)
+            stub_native_calculations_factory = create_native_calculations_factory(
+                warnings=expected_warnings, calculation_unit=units.Metric.PRESSURE,
+                pressure_magnitude=DONT_CARE_MEASUREMENT_MAGNITUDE)
             with self.subTest(expected_warnings=expected_warnings):
                 actual_result = ntc.median_treating_pressure(stub_stage, start_time, stop_time,
                                                              calculations_factory=stub_native_calculations_factory)
