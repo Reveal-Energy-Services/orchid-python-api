e53e649c80abdf4184668b58a25cc00031f504f5
Larry Jones <larry.jones@reveal-energy.com>
2020-12-12 23:25:13 -0600

    Use Google docstring format

--- orchid/measurement.py
+++ orchid/measurement.py
@@ -29,45 +29,55 @@ import orchid.unit_system as units
 Measurement = collections.namedtuple('measurement', ['magnitude', 'unit'], module=__name__)
 
 
-def get_conversion_factor(source_unit, target_unit):
+def get_conversion_factor(source_unit: Union[units.UsOilfield, units.Metric],
+                          target_unit: Union[units.UsOilfield, units.Metric]) -> float:
+    """
+    Returns the conversion factor from `source_unit` to `target_unit`.
+
+    Args:
+        source_unit: The source unit for the conversion.
+        target_unit: The target unit for the conversion.
+    """
     pass
 
 
-def make_measurement(magnitude: Union[int, float], unit_text: Union[units.UsOilfield, units.Metric]) -> Measurement:
+def make_measurement(magnitude: numbers.Real, unit: Union[units.UsOilfield, units.Metric]) -> Measurement:
     """
     Construct a measurement.
 
     Args:
         magnitude: The magnitude of the measurement.
-        unit_text: An abbreviation for the unit of this measurement.
+        unit: The unit of this measurement.
 
     Returns:
-        The measurement consisting of a magnitude and a unit.
+        The Measurement consisting of the supplied `magnitude` and `unit`.
     """
     pass
 
 
-def slurry_rate_volume_unit(slurry_rate_unit: str) -> str:
+def slurry_rate_volume_unit(
+        slurry_rate_unit: Union[units.UsOilfield, units.Metric]) -> Union[units.UsOilfield, units.Metric]:
     """
-    Extract the volume unit from the compound `slurry_rate_unit`.
+    Extract the volume unit from the `slurry_rate_unit`.
 
     Args:
-        slurry_rate_unit: The abbreviation for the compound slurry rate unit.
-Q
+        slurry_rate_unit: The slurry rate unit.
+
     Returns:
-        The abbreviation for the volume unit of the slurry rate unit.
+        The volume unit of the slurry rate unit.
     """
     pass
 
 
-def proppant_concentration_mass_unit(proppant_concentration_unit: str) -> str:
+def proppant_concentration_mass_unit(
+        proppant_concentration_unit: Union[units.UsOilfield, units.Metric]) -> Union[units.UsOilfield, units.Metric]:
     """
-    Extract the mass unit from the compound `proppant_concentration_unit`.
+    Extract the mass unit from the `proppant_concentration_unit`.
 
     Args:
-        proppant_concentration_unit: The abbreviation for the proppant concentration unit.
+        proppant_concentration_unit: The proppant concentration unit.
 
     Returns:
-        The abbreviation for the mass unit of the proppant concentration unit.
+        The mass unit of the proppant concentration unit.
     """
     pass
