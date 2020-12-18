0a2dad675e6299d8d9e319f073000a351c80a4e1
Larry Jones <larry.jones@reveal-energy.com>
2020-12-18 00:39:57 -0600

    Final elimination of duplicate test code

--- tests/test_native_treatment_calculations.py
+++ tests/test_native_treatment_calculations.py
@@ -47,16 +47,14 @@ class TestNativeTreatmentCalculationsAdapter(unittest.TestCase):
 
     def test_median_treating_pressure_returns_get_median_treating_pressure_result(self):
         for expected_magnitude, unit in [(7396.93, units.UsOilfield.PRESSURE), (74.19, units.Metric.PRESSURE)]:
-            warnings = []
             expected_measurement = om.make_measurement(expected_magnitude, unit)
 
-            sut = ntc.median_treating_pressure
-            start_time = datetime.datetime(2023, 7, 2, 3, 57, 19)
-            stop_time = datetime.datetime(2023, 7, 2, 5, 30, 2)
-
-            stub_calculation_result = create_stub_calculation_result(expected_measurement, warnings)
+            stub_calculation_result = create_stub_calculation_result(expected_measurement, DONT_CARE_WARNINGS)
             stub_treatment_calculations = create_stub_treatment_pressure_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.median_treating_pressure,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2023, 7, 2, 3, 57, 19),
+                                                    datetime.datetime(2023, 7, 2, 5, 30, 2),
                                                     expected_measurement=expected_measurement)
 
     def assert_expected_calculation_result(self, sut, stub_treatment_calculations, start_time, stop_time,
@@ -76,69 +74,62 @@ class TestNativeTreatmentCalculationsAdapter(unittest.TestCase):
         for expected_warnings in [[], ['lente vetustas lupam vicit'], ['clunis', 'nobile', 'complacuit']]:
             dont_care_measurement = om.make_measurement(DONT_CARE_MEASUREMENT_MAGNITUDE, units.UsOilfield.PRESSURE)
 
-            sut = ntc.median_treating_pressure
-            start_time = datetime.datetime(2023, 7, 2, 3, 57, 19)
-            stop_time = datetime.datetime(2023, 7, 2, 5, 30, 2)
-
             stub_calculation_result = create_stub_calculation_result(dont_care_measurement, expected_warnings)
             stub_treatment_calculations = create_stub_treatment_pressure_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.median_treating_pressure,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2023, 7, 2, 3, 57, 19),
+                                                    datetime.datetime(2023, 7, 2, 5, 30, 2),
                                                     expected_warnings=expected_warnings)
 
     def test_pumped_fluid_volume_returns_get_pumped_volume_result(self):
         for expected_magnitude, unit in [(6269.20, units.UsOilfield.VOLUME), (707.82, units.Metric.VOLUME)]:
-            warnings = []
             expected_measurement = om.make_measurement(expected_magnitude, unit)
 
-            sut = ntc.pumped_fluid_volume
-            start_time = datetime.datetime(2023, 8, 6, 3, 52, 4)
-            stop_time = datetime.datetime(2023, 8, 6, 5, 8, 20)
-
-            stub_calculation_result = create_stub_calculation_result(expected_measurement, warnings)
+            stub_calculation_result = create_stub_calculation_result(expected_measurement, DONT_CARE_WARNINGS)
             stub_treatment_calculations = create_stub_pumped_volume_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.pumped_fluid_volume,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2023, 8, 6, 3, 52, 4),
+                                                    datetime.datetime(2023, 8, 6, 5, 8, 20),
                                                     expected_measurement=expected_measurement)
 
     def test_pumped_fluid_volume_returns_get_pumped_volume_warnings(self):
         for expected_warnings in [['urinator egregrius'], ['nomenclatura', 'gestus', 'tertia'], []]:
             dont_care_measurement = om.make_measurement(DONT_CARE_MEASUREMENT_MAGNITUDE, units.UsOilfield.VOLUME)
 
-            sut = ntc.pumped_fluid_volume
-            start_time = datetime.datetime(2023, 8, 6, 3, 52, 4)
-            stop_time = datetime.datetime(2023, 8, 6, 5, 8, 20)
-
             stub_calculation_result = create_stub_calculation_result(dont_care_measurement, expected_warnings)
             stub_treatment_calculations = create_stub_pumped_volume_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.pumped_fluid_volume,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2023, 8, 6, 3, 52, 4),
+                                                    datetime.datetime(2023, 8, 6, 5, 8, 20),
                                                     expected_warnings=expected_warnings)
 
     def test_total_proppant_mass_returns_get_total_proppant_mass_result(self):
         for expected_magnitude, unit in [(5414.58, units.UsOilfield.MASS), (138262.86, units.Metric.MASS)]:
-            warnings = []
             expected_measurement = om.make_measurement(expected_magnitude, unit)
 
-            sut = ntc.total_proppant_mass
-            start_time = datetime.datetime(2020, 1, 29, 7, 35, 2)
-            stop_time = datetime.datetime(2020, 1, 29, 9, 13, 30)
-
-            stub_calculation_result = create_stub_calculation_result(expected_measurement, warnings)
+            stub_calculation_result = create_stub_calculation_result(expected_measurement, DONT_CARE_WARNINGS)
             stub_treatment_calculations = create_stub_proppant_mass_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.total_proppant_mass,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2020, 1, 29, 7, 35, 2),
+                                                    datetime.datetime(2020, 1, 29, 9, 13, 30),
                                                     expected_measurement=expected_measurement)
 
     def test_total_proppant_mass_returns_get_total_proppant_mass_warnings(self):
         for expected_warnings in [[],  ['igitur', 'pantinam', 'incidi'], ['violentia venio']]:
             dont_care_measurement = om.make_measurement(DONT_CARE_MEASUREMENT_MAGNITUDE, units.UsOilfield.VOLUME)
 
-            sut = ntc.total_proppant_mass
-            start_time = datetime.datetime(2020, 1, 29, 7, 35, 2)
-            stop_time = datetime.datetime(2020, 1, 29, 9, 13, 30)
-
             stub_calculation_result = create_stub_calculation_result(dont_care_measurement, expected_warnings)
             stub_treatment_calculations = create_stub_proppant_mass_calculation(stub_calculation_result)
-            self.assert_expected_calculation_result(sut, stub_treatment_calculations, start_time, stop_time,
+            self.assert_expected_calculation_result(ntc.total_proppant_mass,
+                                                    stub_treatment_calculations,
+                                                    datetime.datetime(2020, 1, 29, 7, 35, 2),
+                                                    datetime.datetime(2020, 1, 29, 9, 13, 30),
                                                     expected_warnings=expected_warnings)
-            
+
 
 def create_stub_stage_adapter():
     result = unittest.mock.Mock('stub_stage_adapter', autospec=nsa.NativeStageAdapter)
