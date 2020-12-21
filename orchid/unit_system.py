1029243274d2ac33410e43d97eaf2e6ce6b61d84
Larry Jones <larry.jones@reveal-energy.com>
2020-12-21 13:13:12 -0600

    Expose Pint units via unit_system directly
    Previous commits required consumers to use the registry; for example,
    using meters would require the expression `units.registry.m` (even
    worse for ratio units).
    
    This commit hides the created Pint `UnitRegistry` instance but exposes
    units using a simpler expression. For example, `units.m` or `units.kPa`.
    Additionally, this creates aliases for ratio units.
    
    These changes allow consuming classes to create a unit using expressions
    like `3.14 * units.UsOilfield.LENGTH`.

--- orchid/unit_system.py
+++ orchid/unit_system.py
@@ -26,7 +26,19 @@ import UnitsNet
 # This is the single location to find the `pint.UnitRegistry`. The `pint` package considers units returned
 # from different instances of `UnitRegistry` to be different. See the documentation at
 # https://pint.readthedocs.io/en/stable/tutorial.html#using-pint-in-your-projects for details.
-registry = UnitRegistry()
+_registry = UnitRegistry()
+
+# Expose general types for use by type annotations
+Quantity = _registry.Quantity
+Unit = _registry.Unit
+
+# Aliases for "ratio" units (proppant concentration and slurry rate). Although a consuming class can use an
+# expression like `2.718 * units.oil_bbl_per_min` (or even, `2.718 * (units.oil_bbl / units.min); the preferred
+# method is to use the expression, `2.718 * units.UsOilfield.PROPPANT_CONCENTRATION`.
+lb_per_gal = (_registry.lb / _registry.gal)
+kg_per_cu_m = (_registry.kg / (_registry.m ** 3))
+oil_bbl_per_min = (_registry.oil_bbl / _registry.min)
+cu_m_per_min = ((_registry.m ** 3) / _registry.min)
 
 
 About = namedtuple('About', ['unit', 'net_unit'])
@@ -66,8 +78,6 @@ DURATION = _Duration()
 
 
 class UnitSystem(Enum):
-    net_unit = property(lambda self: self.value.net_unit)
-
     def __repr__(self):
         return f'<{self.system_name()}: {str(self.value)}>'
 
@@ -99,14 +109,13 @@ class UnitSystem(Enum):
 class UsOilfield(UnitSystem):
     """The enumeration of U. S. oilfield units available via the Orchid Python API."""
 
-    LENGTH = About('ft', UnitsNet.Units.LengthUnit.Foot)
-    MASS = About('lb', UnitsNet.Units.MassUnit.Pound)
-    PRESSURE = About('psi', UnitsNet.Units.PressureUnit.PoundForcePerSquareInch)
-    PROPPANT_CONCENTRATION = About('lbs/gal (US)',
-                                   (UnitsNet.Units.MassUnit.Pound, UnitsNet.Units.VolumeUnit.OilBarrel))
-    SLURRY_RATE = About('bbl/min', (UnitsNet.Units.VolumeUnit.OilBarrel, UnitsNet.Units.DurationUnit.Minute))
-    TEMPERATURE = About('F', UnitsNet.Units.TemperatureUnit.DegreeFahrenheit)
-    VOLUME = About('bbl', UnitsNet.Units.VolumeUnit.OilBarrel)
+    LENGTH = _registry.ft
+    MASS = _registry.lb
+    PRESSURE = _registry.psi
+    PROPPANT_CONCENTRATION = lb_per_gal
+    SLURRY_RATE = oil_bbl_per_min
+    TEMPERATURE = _registry.degF
+    VOLUME = _registry.oil_bbl
 
     def system_name(self):
         return 'USOilfield'
@@ -115,14 +124,13 @@ class UsOilfield(UnitSystem):
 class Metric(UnitSystem):
     """The enumeration of metric units available via the Orchid Python API."""
 
-    LENGTH = About('m', UnitsNet.Units.LengthUnit.Meter)
-    MASS = About('kg', UnitsNet.Units.MassUnit.Kilogram)
-    PRESSURE = About('kPa', UnitsNet.Units.PressureUnit.Kilopascal)
-    PROPPANT_CONCENTRATION = About('kg/m^3',
-                                   (UnitsNet.Units.MassUnit.Kilogram, UnitsNet.Units.VolumeUnit.CubicMeter))
-    SLURRY_RATE = About('m^3/min', (UnitsNet.Units.VolumeUnit.CubicMeter, UnitsNet.Units.DurationUnit.Minute))
-    TEMPERATURE = About('C', UnitsNet.Units.TemperatureUnit.DegreeCelsius)
-    VOLUME = About('m^3', UnitsNet.Units.VolumeUnit.CubicMeter)
+    LENGTH = _registry.m
+    MASS = _registry.kg
+    PRESSURE = _registry.kPa
+    PROPPANT_CONCENTRATION = kg_per_cu_m
+    SLURRY_RATE = cu_m_per_min
+    TEMPERATURE = _registry.degC
+    VOLUME = (_registry.m ** 3)
 
     def system_name(self):
         return 'Metric'
