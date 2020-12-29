2b0fa998bafb95634b41130069f05a64c9055fc5
Larry Jones <larry.jones@reveal-energy.com>
2020-12-28 18:21:27 -0600

    Expand tests for as_measurement
    This commit expands the unit tests for `net_quantity.as_measurement` to
    include `ANGLE`, `ENERGY` and 'FORCE`.

--- orchid/net_quantity.py
+++ orchid/net_quantity.py
@@ -22,8 +22,7 @@ import datetime
 
 import toolz.curried as toolz
 
-from orchid import (measurement as om,
-                    physical_quantity as opq,
+from orchid import (physical_quantity as opq,
                     unit_system as units)
 
 # noinspection PyUnresolvedReferences
@@ -34,8 +33,10 @@ from System import DateTime
 import UnitsNet
 
 
-UNIT_CREATE_NET_UNIT_MAP = {
+_UNIT_CREATE_NET_UNIT_MAP = {
+    units.common[opq.ANGLE]: lambda q: UnitsNet.Angle.FromDegrees(q),
     units.common[opq.DURATION]: lambda q: UnitsNet.Duration.FromMinutes(q),
+    units.us_oilfield[opq.DENSITY]: lambda q: UnitsNet.Density.FromPoundsPerCubicFoot(q),
     units.us_oilfield[opq.LENGTH]: lambda q: UnitsNet.Length.FromFeet(q),
     units.metric[opq.LENGTH]: lambda q: UnitsNet.Length.FromMeters(q),
     units.us_oilfield[opq.MASS]: lambda q: UnitsNet.Mass.FromPounds(q),
@@ -44,6 +45,18 @@ UNIT_CREATE_NET_UNIT_MAP = {
     units.metric[opq.PRESSURE]: lambda q: UnitsNet.Pressure.FromKilopascals(q),
     units.us_oilfield[opq.VOLUME]: lambda q: UnitsNet.Volume.FromOilBarrels(q),
     units.metric[opq.VOLUME]: lambda q: UnitsNet.Volume.FromCubicMeters(q),
+}
+
+_UNIT_NET_UNIT_MAP = {
+    units.common[opq.DURATION]: UnitsNet.Units.DurationUnit.Minute,
+    units.us_oilfield[opq.LENGTH]: UnitsNet.Units.LengthUnit.Foot,
+    units.metric[opq.LENGTH]: UnitsNet.Units.LengthUnit.Meter,
+    units.us_oilfield[opq.MASS]: UnitsNet.Units.MassUnit.Pound,
+    units.metric[opq.MASS]: UnitsNet.Units.MassUnit.Kilogram,
+    units.us_oilfield[opq.PRESSURE]: UnitsNet.Units.PressureUnit.PoundForcePerSquareInch,
+    units.metric[opq.PRESSURE]: UnitsNet.Units.PressureUnit.Kilopascal,
+    units.us_oilfield[opq.VOLUME]: UnitsNet.Units.VolumeUnit.OilBarrel,
+    units.metric[opq.VOLUME]: UnitsNet.Units.VolumeUnit.CubicMeter,
 }
 
 
@@ -62,17 +75,22 @@ def as_datetime(net_time_point: DateTime) -> datetime.datetime:
                              net_time_point.Millisecond * 1000)
 
 
-def as_measurement(net_quantity: UnitsNet.IQuantity) -> units.Quantity:
+def as_measurement(net_quantity: UnitsNet.IQuantity,
+                   physical_quantity: opq.PhysicalQuantity) -> units.Quantity:
     """
     Convert a .NET UnitsNet.IQuantity to a `Measurement` instance.
 
     Args:
         net_quantity: The .NET IQuantity instance to convert.
+        physical_quantity: The optional `PhysicalQuantity`. Although we try to determine a unique mapping
+        between units in the `unit_system` module and .NET `UnitsNet` units, we cannot perform a unique
+        mapping for density and proppant concentration measured in the metric system (the units of these
+        physical quantities are "kg/m**3"".
 
     Returns:
         The equivalent `Measurement` instance.
     """
-    result = net_quantity.Value * _unit_from_net_quantity(net_quantity)
+    result = net_quantity.Value * _unit_from_net_quantity(net_quantity, physical_quantity)
     return result
 
 
@@ -90,44 +108,53 @@ def as_net_date_time(time_point: datetime.datetime) -> DateTime:
                     time_point.second, round(time_point.microsecond / 1e3))
 
 
-def as_net_quantity(measurement: om.Measurement) -> UnitsNet.IQuantity:
+def as_net_quantity(measurement: units.Quantity) -> UnitsNet.IQuantity:
     """
-    Convert a `Measurement` instance to a .NET `UnitsNet.IQuantity` instance.
+    Convert a `Quantity` instance to a .NET `UnitsNet.IQuantity` instance.
 
     Args:
-        measurement: The `Measurement` instance to convert.
+        measurement: The `Quantity` instance to convert.
 
     Returns:
         The equivalent `UnitsNet.IQuantity` instance.
     """
     quantity = UnitsNet.QuantityValue.op_Implicit(measurement.magnitude)
     try:
-        return UNIT_CREATE_NET_UNIT_MAP[measurement.unit](quantity)
+        return _UNIT_CREATE_NET_UNIT_MAP[measurement.units](quantity)
     except KeyError:
-        if measurement.unit == units.UsOilfield.PROPPANT_CONCENTRATION or \
-                measurement.unit == units.Metric.PROPPANT_CONCENTRATION:
-            return ProppantConcentration(measurement.magnitude, measurement.unit.value.net_unit[0],
-                                         measurement.unit.value.net_unit[1])
-        elif measurement.unit == units.UsOilfield.SLURRY_RATE or measurement.unit == units.Metric.SLURRY_RATE:
-            return SlurryRate(measurement.magnitude, measurement.unit.value.net_unit[0],
-                              measurement.unit.value.net_unit[1])
-            pass
+        if measurement.units == units.us_oilfield[opq.PROPPANT_CONCENTRATION]:
+            return ProppantConcentration(measurement.magnitude,
+                                         UnitsNet.Units.MassUnit.Pound,
+                                         UnitsNet.Units.VolumeUnit.UsGallon)
+        elif measurement.units == units.metric[opq.PROPPANT_CONCENTRATION]:
+            return ProppantConcentration(measurement.magnitude,
+                                         UnitsNet.Units.MassUnit.Kilogram,
+                                         UnitsNet.Units.VolumeUnit.CubicMeter)
+        elif measurement.units == units.us_oilfield[opq.SLURRY_RATE]:
+            return SlurryRate(measurement.magnitude,
+                              UnitsNet.Units.VolumeUnit.OilBarrel,
+                              UnitsNet.Units.DurationUnit.Minute)
+        elif measurement.units == units.metric[opq.SLURRY_RATE]:
+            return SlurryRate(measurement.magnitude,
+                              UnitsNet.Units.VolumeUnit.CubicMeter,
+                              UnitsNet.Units.DurationUnit.Minute)
         else:
             raise ValueError(f'Unrecognized unit: "{measurement.unit}".')
 
 
-def as_net_quantity_in_different_unit(measurement: om.Measurement, target_unit: units.Unit) -> UnitsNet.IQuantity:
+def as_net_quantity_in_different_unit(measurement: units.Quantity, target_unit: units.Unit) -> UnitsNet.IQuantity:
     """
-    Convert a `Measurement` into a .NET `UnitsNet.IQuantity` instance but in a different unit, `in_unit`.
+    Convert a `Quantity` into a .NET `UnitsNet.IQuantity` instance but in a different unit, `in_unit`.
+
     Args:
-        measurement: The `Measurement` instance to convert.
-        target_unit: The target unit of the converted `Measurement`.
+        measurement: The `Quantity` instance to convert.
+        target_unit: The target unit of the converted `Quantity`.
 
     Returns:
         The  `NetUnits.IQuantity` instance equivalent to `measurement`.
     """
     net_to_convert = as_net_quantity(measurement)
-    return net_to_convert.ToUnit(target_unit.value.net_unit)
+    return net_to_convert.ToUnit(_UNIT_NET_UNIT_MAP[target_unit])
 
 
 @toolz.curry
@@ -206,26 +233,43 @@ _NET_ABBREVIATION_UNIT_MAP = {
 }
 
 
-def _unit_from_net_quantity(net_quantity):
-    if _is_minute_unit(net_quantity):
-        return units.common[opq.DURATION]
-    elif _is_ratio_unit(net_quantity):
-        numerator_unit, denominator_unit = _ratio_units(net_quantity)
-        if numerator_unit == UnitsNet.Units.MassUnit.Pound and denominator_unit == UnitsNet.Units.VolumeUnit.UsGallon:
-            return units.us_oilfield[opq.PROPPANT_CONCENTRATION]
-        elif numerator_unit == UnitsNet.Units.MassUnit.Kilogram and \
-                denominator_unit == UnitsNet.Units.VolumeUnit.CubicMeter:
-            return units.metric[opq.PROPPANT_CONCENTRATION]
-        elif numerator_unit == UnitsNet.Units.VolumeUnit.OilBarrel and\
-                denominator_unit == UnitsNet.Units.DurationUnit.Minute:
-            return units.us_oilfield[opq.SLURRY_RATE]
-        elif numerator_unit == UnitsNet.Units.VolumeUnit.CubicMeter and \
-                denominator_unit == UnitsNet.Units.DurationUnit.Minute:
-            return units.metric[opq.SLURRY_RATE]
-    else:
-        # Because UnitsNet uses an enumeration for each unit within a physical quantity (length, mass, and so
-        # on) because Python.Net converts all .NET enumerations into Python `int`s, I cannot simply "dispatch'
-        # on the UnitsNet Unit. Consequently, I "dispatch" on the UnitsNet unit abbreviation.
-        net_unit_abbreviation = str(net_quantity).split(maxsplit=1)[1]
-        result = _NET_ABBREVIATION_UNIT_MAP[net_unit_abbreviation]
-        return result
+_NET_UNIT_UNIT_MAP = {
+    (UnitsNet.Units.AngleUnit.Degree, opq.PhysicalQuantity.ANGLE): units.common[opq.ANGLE],
+    (UnitsNet.Units.DurationUnit.Minute, opq.PhysicalQuantity.DURATION): units.common[opq.DURATION],
+    (UnitsNet.Units.DensityUnit.PoundPerCubicFoot, opq.PhysicalQuantity.DENSITY): units.us_oilfield[opq.DENSITY],
+    (UnitsNet.Units.DensityUnit.KilogramPerCubicMeter, opq.PhysicalQuantity.DENSITY): units.metric[opq.DENSITY],
+    (UnitsNet.Units.EnergyUnit.FootPound, opq.PhysicalQuantity.ENERGY): units.us_oilfield[opq.ENERGY],
+    (UnitsNet.Units.EnergyUnit.Joule, opq.PhysicalQuantity.ENERGY): units.metric[opq.ENERGY],
+    (UnitsNet.Units.ForceUnit.PoundForce, opq.PhysicalQuantity.FORCE): units.us_oilfield[opq.FORCE],
+    # (UnitsNet.Units.ForceUnit.Newton, opq.PhysicalQuantity.FORCE): units.metric[opq.FORCE],
+    (UnitsNet.Units.ForceUnit.Newton, opq.PhysicalQuantity.FORCE): units.us_oilfield[opq.FORCE],
+    (UnitsNet.Units.LengthUnit.Foot, opq.PhysicalQuantity.LENGTH): units.us_oilfield[opq.LENGTH],
+    (UnitsNet.Units.LengthUnit.Meter, opq.PhysicalQuantity.LENGTH): units.metric[opq.LENGTH],
+}
+
+
+def _unit_from_net_quantity(net_quantity, physical_quantity):
+    result = _NET_UNIT_UNIT_MAP[(net_quantity.Unit, physical_quantity)]
+    return result
+    # if _is_minute_unit(net_quantity):
+    #     return units.common[opq.DURATION]
+    # elif _is_ratio_unit(net_quantity):
+    #     numerator_unit, denominator_unit = _ratio_units(net_quantity)
+    #     if numerator_unit == UnitsNet.Units.MassUnit.Pound and denominator_unit == UnitsNet.Units.VolumeUnit.UsGallon:
+    #         return units.us_oilfield[opq.PROPPANT_CONCENTRATION]
+    #     elif numerator_unit == UnitsNet.Units.MassUnit.Kilogram and \
+    #             denominator_unit == UnitsNet.Units.VolumeUnit.CubicMeter:
+    #         return units.metric[opq.PROPPANT_CONCENTRATION]
+    #     elif numerator_unit == UnitsNet.Units.VolumeUnit.OilBarrel and\
+    #             denominator_unit == UnitsNet.Units.DurationUnit.Minute:
+    #         return units.us_oilfield[opq.SLURRY_RATE]
+    #     elif numerator_unit == UnitsNet.Units.VolumeUnit.CubicMeter and \
+    #             denominator_unit == UnitsNet.Units.DurationUnit.Minute:
+    #         return units.metric[opq.SLURRY_RATE]
+    # else:
+    #     # Because UnitsNet uses an enumeration for each unit within a physical quantity (length, mass, and so
+    #     # on) because Python.Net converts all .NET enumerations into Python `int`s, I cannot simply "dispatch'
+    #     # on the UnitsNet Unit. Consequently, I "dispatch" on the UnitsNet unit abbreviation.
+    #     net_unit_abbreviation = str(net_quantity).split(maxsplit=1)[1]
+    #     result = _NET_ABBREVIATION_UNIT_MAP[net_unit_abbreviation]
+    #     return result
