b17ce0dd7a82093113798674393a99886f04d27f
Larry Jones <larry.jones@reveal-energy.com>
2021-01-11 18:46:13 -0600

    Move as_unit_system to more accessible location
    Previous commits put the function, `as_unit_system`, which converted a
    .NET `UnitSystem` to a Python `UnitSystem`, in the `project` module
    because it was only needed from that location.
    
    This commit moves that function to `unit_system` to make it accessible
    to any code which has access to the .NET `UnitSystem` and to the
    `unit_system` module (the latter is true for most modules).
    Additionally, since `as_unit_system` returns a `UnitSystem` enumeration
    (defined in the `unit_system` module already, this move reduces coupling
    between modules.

--- orchid/project.py
+++ orchid/project.py
@@ -20,25 +20,20 @@ from typing import List, Tuple, Iterable
 import deal
 import toolz.curried as toolz
 
-import orchid.dot_net_dom_access as dna
-from orchid.native_well_adapter import NativeWellAdapter
-from orchid.native_monitor_curve_adapter import NativeMonitorCurveAdapter
+from orchid import (
+    dot_net_dom_access as dna,
+    native_well_adapter as nwa,
+    native_monitor_curve_adapter as mca,
+    unit_system as units,
+)
 from orchid.project_loader import ProjectLoader
-import orchid.unit_system as units
 
 # noinspection PyUnresolvedReferences
 from Orchid.FractureDiagnostics import IWell, UnitSystem
 # noinspection PyUnresolvedReferences
 import UnitsNet
 
-
-def as_unit_system(net_unit_system: UnitSystem):
-    if net_unit_system == UnitSystem.USOilfield():
-        return units.UsOilfield
-    elif net_unit_system == UnitSystem.Metric():
-        return units.Metric
-    else:
-        raise ValueError(f'Unrecognized unit system: {net_unit_system}')
+from orchid.unit_system import as_unit_system
 
 
 class Project(dna.DotNetAdapter):
@@ -59,7 +54,7 @@ class Project(dna.DotNetAdapter):
     name = dna.dom_property('name', 'The name of this project.')
     project_units = dna.transformed_dom_property('project_units', 'The project unit system.', as_unit_system)
     wells = dna.transformed_dom_property_iterator('wells', 'An iterator of all the wells in this project.',
-                                                  NativeWellAdapter)
+                                                  nwa.NativeWellAdapter)
 
     def default_well_colors(self) -> List[Tuple[float, float, float]]:
         """
@@ -69,7 +64,7 @@ class Project(dna.DotNetAdapter):
         result = list(map(tuple, self._project_loader.native_project().PlottingSettings.GetDefaultWellColors()))
         return result
 
-    def monitor_curves(self) -> Iterable[NativeMonitorCurveAdapter]:
+    def monitor_curves(self) -> Iterable[mca.NativeMonitorCurveAdapter]:
         """
             Return a sequence of well time series for this project.
         Returns:
@@ -77,7 +72,7 @@ class Project(dna.DotNetAdapter):
         """
         native_time_series_list_items = self._project_loader.native_project().WellTimeSeriesList.Items
         if len(native_time_series_list_items) > 0:
-            return toolz.map(NativeMonitorCurveAdapter,
+            return toolz.map(mca.NativeMonitorCurveAdapter,
                              self._project_loader.native_project().WellTimeSeriesList.Items)
         else:
             return []
