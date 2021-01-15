6fc067be5df74cb82a95df1cf86a674441acb803
Larry Jones <larry.jones@reveal-energy.com>
2021-01-15 12:40:28 -0600

    RELEASE notes; methods ordered alphabetically

--- orchid/native_stage_adapter.py
+++ orchid/native_stage_adapter.py
@@ -275,6 +275,12 @@ class NativeStageAdapter(dna.DotNetAdapter):
         return subsurface_point_in_length_unit(depth_datum, xy_reference_frame, in_length_unit,
                                                stage_location_cluster_func)
 
+    @deal.pre(validation.arg_is_acceptable_pressure_unit)
+    def isip_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
+        net_isip = self._adaptee.Isip
+        net_isip_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_isip)
+        return onq.as_pressure_measurement(net_isip_correct_units)
+
     def md_top(self, in_length_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
         """
         Return the measured depth of the top of this stage (closest to the well head / farthest from the toe)
@@ -306,6 +312,18 @@ class NativeStageAdapter(dna.DotNetAdapter):
         md_top_quantity = onq.convert_net_quantity_to_different_unit(in_length_unit, original)
         result = onq.as_length_measurement(md_top_quantity)
         return result
+
+    @deal.pre(validation.arg_is_acceptable_pressure_unit)
+    def pnet_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
+        net_pnet = self._adaptee.Pnet
+        net_pnet_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_pnet)
+        return onq.as_pressure_measurement(net_pnet_correct_units)
+
+    @deal.pre(validation.arg_is_acceptable_pressure_unit)
+    def shmin_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
+        net_shmin = self._adaptee.Shmin
+        net_shmin_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_shmin)
+        return onq.as_pressure_measurement(net_shmin_correct_units)
 
     def stage_length(self, in_length_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
         """
@@ -366,21 +384,3 @@ class NativeStageAdapter(dna.DotNetAdapter):
                             # Transform the map to a dictionary keyed by the sampled quantity name
                             lambda cs: toolz.reduce(add_curve, cs, {}))
         return result
-
-    @deal.pre(validation.arg_is_acceptable_pressure_unit)
-    def isip_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
-        net_isip = self._adaptee.Isip
-        net_isip_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_isip)
-        return onq.as_pressure_measurement(net_isip_correct_units)
-
-    @deal.pre(validation.arg_is_acceptable_pressure_unit)
-    def pnet_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
-        net_pnet = self._adaptee.Pnet
-        net_pnet_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_pnet)
-        return onq.as_pressure_measurement(net_pnet_correct_units)
-
-    @deal.pre(validation.arg_is_acceptable_pressure_unit)
-    def shmin_in_pressure_unit(self, target_unit: Union[units.UsOilfield, units.Metric]) -> om.Measurement:
-        net_shmin = self._adaptee.Shmin
-        net_shmin_correct_units = onq.convert_net_quantity_to_different_unit(target_unit, net_shmin)
-        return onq.as_pressure_measurement(net_shmin_correct_units)
