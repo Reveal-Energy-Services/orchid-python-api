28c5a3ca4eb14db843d80422c3d9fcefd91b6817
Larry Jones <larry.jones@reveal-energy.com>
2021-03-10 17:22:09 -0600

    Extract ScriptAdapterContext into its own module
    Previous commits included the `ScriptAdapterContext` in the `dot_net`
    module.
    
    This commit moves the class definition into the module
    `script_adapter_context`.

--- orchid/dot_net.py
+++ orchid/dot_net.py
@@ -18,67 +18,14 @@
 
 import os
 import pathlib
-import sys
 
 import orchid.configuration
+import orchid.script_adatper_context as sac
 
 import toolz.curried as toolz
 
 # noinspection PyPackageRequirements
 import clr
-# noinspection PyUnresolvedReferences
-from System import InvalidOperationException
-
-
-def append_orchid_assemblies_directory_path() -> None:
-    """
-    Append the directory containing the required Orchid assemblies to `sys.path`.
-    """
-    orchid_bin_dir = toolz.get_in(['orchid', 'root'], orchid.configuration.python_api())
-    if orchid_bin_dir not in sys.path:
-        sys.path.append(orchid_bin_dir)
-
-
-append_orchid_assemblies_directory_path()
-
-clr.AddReference('Orchid.FractureDiagnostics.SDKFacade')
-# noinspection PyUnresolvedReferences
-from Orchid.FractureDiagnostics.SDKFacade import ScriptAdapter
-
-
-class ScriptAdapterContext:
-    """
-    A "private" class with the responsibility to initialize and shutdown the .NET ScriptAdapter class.
-
-    I considered making `ProjectLoader` a context manager; however, the API then becomes somewhat unclear.
-    - Does the constructor enter the context? Must a caller initialize the instance and then enter the
-      context?
-    - What results if a caller *does not* enter the context?
-    - Enters the context twice?
-
-    Because I was uncertain I created this private class to model the `ScriptAdapter` context. The property,
-    `ProjectLoader.native_project`, enters the context if it will actually read the project and exits the
-    context when the read operation is finished.
-
-    For information on Python context managers, see
-    [the Python docs](https://docs.python.org/3.8/library/stdtypes.html#context-manager-types)
-    """
-
-    def __enter__(self):
-        try:
-            ScriptAdapter.Init()
-            return self
-        except InvalidOperationException as ioe:
-            if 'REVEAL-CORE-0xDEADFA11' in ioe.Message:
-                print('Orchid licensing error. Please contact Orchid technical support.')
-                sys.exit(-1)
-            else:
-                raise
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        ScriptAdapter.Shutdown()
-        # Returning no value will propagate the exception to the caller in the normal way
-        return
 
 
 def add_orchid_assemblies() -> None:
@@ -108,12 +55,10 @@ def app_settings_path() -> str:
 
 
 def prepare_imports() -> None:
-    orchid.dot_net.append_orchid_assemblies_directory_path()
-
     # This function call must occur *after*
     # - Importing clr
     # - Adding a reference to `Orchid.FractureDiagnostics.SDKFacade`
     # - Importing ScriptAdapter from `Orchid.FractureDiagnostics.SDKFacade`
     # - The call to `append_orchid_assemblies_directory_path`
-    with ScriptAdapterContext():
+    with sac.ScriptAdapterContext():
         orchid.dot_net.add_orchid_assemblies()
