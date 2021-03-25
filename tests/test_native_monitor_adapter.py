8c73160ec216e56b0cfdf9a018a691dd5da96626
Larry Jones <larry.jones@reveal-energy.com>
2021-03-25 00:40:12 -0500

    Skeleton TestNativeMonitorAdapter

---
+++ tests/test_native_monitor_adapter.py
@@ -0,0 +1,26 @@
+#
+# This file is part of Orchid and related technologies.
+#
+# Copyright (c) 2017-2021 Reveal Energy Services.  All Rights Reserved.
+#
+# LEGAL NOTICE:
+# Orchid contains trade secrets and otherwise confidential information
+# owned by Reveal Energy Services. Access to and use of this information is 
+# strictly limited and controlled by the Company. This file may not be copied,
+# distributed, or otherwise disclosed outside of the Company's facilities 
+# except under appropriate precautions to maintain the confidentiality hereof, 
+# and may not be used in any way not expressly authorized by the Company.
+#
+
+import unittest
+
+from hamcrest import assert_that, equal_to
+
+
+class TestNativeMonitorAdapter(unittest.TestCase):
+    def test_canary(self):
+        assert_that(2 + 2, equal_to(4))
+
+
+if __name__ == '__main__':
+    unittest.main()
