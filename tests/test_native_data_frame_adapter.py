d9a5df9b340591708d214fd0916b5bfae178c147
Larry Jones <larry.jones@reveal-energy.com>
2021-05-11 01:04:14 -0500

    Reduce duplicated test setup code

--- tests/test_native_data_frame_adapter.py
+++ tests/test_native_data_frame_adapter.py
@@ -59,15 +59,13 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
         assert_that(sut.object_id, equal_to(uuid.UUID('35582fd2-7499-4259-99b8-04b01876f309')))
 
     def test_empty_data_table_produces_empty_pandas_data_frame(self):
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=tsn.TableDataDto([], [], toolz.identity))
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(tsn.TableDataDto([], [], toolz.identity))
 
         pdt.assert_frame_equal(sut.pandas_data_frame(), pd.DataFrame())
 
     def test_single_cell_data_table_produces_correct_pandas_data_frame(self):
         table_data_dto = tsn.TableDataDto([float], [{'oratio': 57.89}], toolz.identity)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
         expected_data_frame = pd.DataFrame(data=toolz.merge_with(toolz.identity, *table_data_dto.table_data),
@@ -77,15 +75,11 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
     def test_single_cell_data_table_with_column_mapping_produces_correct_pandas_data_frame(self):
         rename_column_func = toolz.flip(toolz.get)({'stultus': 'fulmino'})
         table_data_dto = tsn.TableDataDto([str], [{'stultus': 'timeo'}], rename_column_func)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
 
-        expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
-        expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
-        expected_data_frame = pd.DataFrame(data=expected_data,
-                                           columns=expected_columns)
+        expected_data_frame = _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto)
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
 
     def test_single_column_many_rows_data_table_produces_correct_pandas_data_frame(self):
@@ -94,8 +88,7 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
                                            {'rident': 'sapientes'},
                                            {'rident': 'rident'}],
                                           toolz.identity)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
         expected_data_frame = pd.DataFrame(data=toolz.merge_with(toolz.identity, *table_data_dto.table_data),
@@ -109,22 +102,17 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
                                            {'rident': 'sapientes'},
                                            {'rident': 'rident'}],
                                           rename_column_func)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
-        expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
-        expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
-        expected_data_frame = pd.DataFrame(data=expected_data,
-                                           columns=expected_columns)
+        expected_data_frame = _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto)
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
 
     def test_many_columns_single_row_data_table_produces_correct_pandas_data_frame(self):
         table_data_dto = tsn.TableDataDto([str, float, int],
                                           [{'nespila': 'pendet', 'scrupum': -23.14, 'nascor': -116}],
                                           toolz.identity)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
         expected_data_frame = pd.DataFrame(data=toolz.merge_with(toolz.identity, *table_data_dto.table_data),
@@ -138,14 +126,10 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
         table_data_dto = tsn.TableDataDto([float, int, str],
                                           [{'Servius': 3.414, 'onus': 8, 'timoris': 'alescet'}],
                                           rename_column_func)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
-        expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
-        expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
-        expected_data_frame = pd.DataFrame(data=expected_data,
-                                           columns=expected_columns)
+        expected_data_frame = _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto)
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
 
     def test_many_columns_many_rows_data_table_produces_correct_pandas_data_frame(self):
@@ -154,8 +138,7 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
                                            {'cana': 'privat', 'querula': 111, 'recidebimus': 52.02},
                                            {'cana': 'desperant', 'querula': -44, 'recidebimus': 19.52}],
                                           toolz.identity)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
         expected_data_frame = pd.DataFrame(data=toolz.merge_with(toolz.identity, *table_data_dto.table_data),
@@ -169,14 +152,10 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
                                            {'mutabilis': 52, 'lenit': 'privat', 'commoda': 65.52},
                                            {'mutabilis': 36, 'lenit': 'desperant', 'commoda': -95.01}],
                                           rename_column_func)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
-        expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
-        expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
-        expected_data_frame = pd.DataFrame(data=expected_data,
-                                           columns=expected_columns)
+        expected_data_frame = _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto)
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
 
     def test_single_cell_data_table_with_date_time_column_produces_correct_pandas_data_frame(self):
@@ -184,14 +163,10 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
         table_data_dto = tsn.TableDataDto([dt.datetime],
                                           [{'pulchritudo': dup.isoparse('2024-03-15T10:09:18Z')}],
                                           rename_column_func)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
-        expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
-        expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
-        expected_data_frame = pd.DataFrame(data=expected_data,
-                                           columns=expected_columns)
+        expected_data_frame = _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto)
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
 
     def test_many_columns_many_rows_data_table_with_db_null_values_produces_correct_pandas_data_frame(self):
@@ -224,8 +199,7 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
                 },
             ],
             toolz.identity)
-        stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
-        sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+        sut = _create_sut(table_data_dto)
 
         actual_data_frame = sut.pandas_data_frame()
 
@@ -245,6 +219,20 @@ class TestNativeDataFrameAdapter(unittest.TestCase):
         expected_data_frame = pd.DataFrame(data=toolz.merge_with(toolz.identity, *expected_table_data),
                                            columns=toolz.first(table_data_dto.table_data).keys())
         pdt.assert_frame_equal(actual_data_frame, expected_data_frame)
+
+
+def _create_sut(table_data_dto):
+    stub_net_data_frame = tsn.create_stub_net_data_frame(table_data_dto=table_data_dto)
+    sut = dfa.NativeDataFrameAdapter(stub_net_data_frame)
+    return sut
+
+
+def _create_expected_data_frame_with_renamed_columns(rename_column_func, table_data_dto):
+    expected_data = toolz.keymap(rename_column_func, toolz.merge_with(toolz.identity, *table_data_dto.table_data))
+    expected_columns = list(toolz.map(rename_column_func, toolz.first(table_data_dto.table_data).keys()))
+    expected_data_frame = pd.DataFrame(data=expected_data,
+                                       columns=expected_columns)
+    return expected_data_frame
 
 
 if __name__ == '__main__':
