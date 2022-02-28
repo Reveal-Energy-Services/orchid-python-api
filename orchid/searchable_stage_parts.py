73d2b7895c5d13c99f84d85c331b982c9f1db8ee
Larry Jones <larry.jones@reveal-energy.com>
2022-02-27 20:21:44 -0600

    Write method docstrings

--- orchid/searchable_stage_parts.py
+++ orchid/searchable_stage_parts.py
@@ -12,11 +12,30 @@
 # and may not be used in any way not expressly authorized by the Company.
 #
 
-from orchid import searchable_project_objects as spo
+
+from typing import Iterable, Optional
+
+
+from orchid import (
+    native_stage_part_adapter as spa,
+    searchable_project_objects as spo,
+)
 
 
 class SearchableStageParts(spo.SearchableProjectObjects):
-    def find_by_stage_part_number(self, to_find: int):
+    def find_by_stage_part_number(self, to_find: int) -> Optional[spa.NativeStagePartAdapter]:
+        """
+        Find a stage part with the specified part number.
+
+        Args:
+            to_find: The part number sought.
+
+        Returns:
+            The stage part with the specified part number.
+
+            If no such stage part is found, returns `None`. If multiple stage parts with the specified part number are
+            found, raises `spo.SearchableProjectMultipleMatchError`.
+        """
         candidates = list(self.find(lambda s: s.part_no == to_find))
         if len(candidates) == 0:
             return None
@@ -25,8 +44,30 @@ class SearchableStageParts(spo.SearchableProjectObjects):
         else:
             raise spo.SearchableProjectMultipleMatchError(to_find)
 
-    def find_by_display_name_with_well(self, to_find: str):
+    def find_by_display_name_with_well(self, to_find: str) -> Iterable[spa.NativeStagePartAdapter]:
+        """
+        Find a stage part with the specified display name with well.
+
+        Args:
+            to_find: The display name with well sought.
+
+        Returns:
+            The matching stage part(s).
+
+            If no such stage part is in this collection, returns an empty iterator.
+        """
         return self.find(lambda s: s.display_name_with_well == to_find)
 
-    def find_by_display_name_without_well(self, to_find: str):
+    def find_by_display_name_without_well(self, to_find: str) -> Iterable[spa.NativeStagePartAdapter]:
+        """
+        Find a stage part without the specified display name with well.
+
+        Args:
+            to_find: The display name without well sought.
+
+        Returns:
+            The matching stage part(s).
+
+            If no such stage part is in this collection, returns an empty iterator.
+        """
         return self.find(lambda s: s.display_name_without_well == to_find)
