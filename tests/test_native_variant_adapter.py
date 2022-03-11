5c6cfe92dc94acf255eaa82c301dc354c31f8993
Larry Jones <larry.jones@reveal-energy.com>
2022-03-11 12:37:24 -0600

    Initial test ideas for NativeVariantAdapter

--- tests/test_native_variant_adapter.py
+++ tests/test_native_variant_adapter.py
@@ -21,6 +21,23 @@ import unittest
 from hamcrest import assert_that, equal_to
 
 
+# Test ideas
+# - Create int variant
+# - Create double variant
+# - Create str variant
+# - Get value of int variant
+# - Get value of double variant
+# - Get value of str variant
+# - Convert int variant to double
+# - Convert int variant to str
+# - Convert double variant to int
+# - Convert double variant to str
+# - Convert str variant to int
+# - Convert str variant to double
+# - Convert double variant to int fails
+# - Convert str variant to int fails
+# - Convert str variant to double fails
+# TODO Create other variants as needed
 class TestNativeVariantAdapter(unittest.TestCase):
     def test_canary(self):
         assert_that(2 + 2, equal_to(4))
