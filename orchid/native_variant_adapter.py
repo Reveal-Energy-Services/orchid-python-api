1c6e87e9ff6ba01f983f54f131dbd59a6c47b148
Larry Jones <larry.jones@reveal-energy.com>
2022-03-12 15:25:21 -0600

    Write `create_variant`; introduce type

--- orchid/native_variant_adapter.py
+++ orchid/native_variant_adapter.py
@@ -15,16 +15,22 @@
 # This file is part of Orchid and related technologies.
 #
 
+import enum
+from typing import Union
+
 from orchid import dot_net_dom_access as dna
 
 # noinspection PyUnresolvedReferences,PyPackageRequirements
 from Orchid.FractureDiagnostics.Settings import Variant
 # noinspection PyUnresolvedReferences,PyPackageRequirements
-from System import Int32, Type
+from System import Type
 
 
-def make_variant(o):
-    pass
+class PythonVariantTypes(enum.Enum):
+    """Enumerates the recognized Python variant types."""
+    INT32 = Type.GetType('System.Int32')
+    DOUBLE = Type.GetType('System.Double')
+    STRING = Type.GetType('System.String')
 
 
 class NativeVariantAdapter(dna.DotNetAdapter):
@@ -45,5 +51,27 @@ class NativeVariantAdapter(dna.DotNetAdapter):
         super().__init__(adaptee)
 
     @property
-    def value(self):
+    def type(self) -> PythonVariantTypes:
+        """The type of this variant."""
+        return PythonVariantTypes(self.dom_object.Type())
+
+    @property
+    def value(self) -> Union[int, float, str]:
+        """The value of this variant."""
         return self.dom_object.GetValue[self.dom_object.Type()]()
+
+
+def create_variant(value: Union[int, float, str], of_type: PythonVariantTypes) -> NativeVariantAdapter:
+    """
+    Create a new `NativeVariantAdapter` adapting a newly created .NET `Variant`.
+
+    Args:
+
+        value: The value of the new variant.
+        of_type: The type of the new variant.
+
+    Returns:
+        The requested `NativeVariantAdapter'
+    """
+    net_variant = Variant.Create.Overloads[of_type.value](value)
+    return NativeVariantAdapter(net_variant)
--- tests/test_native_variant_adapter.py
+++ tests/test_native_variant_adapter.py
@@ -29,10 +29,23 @@ from System import Int32, Double, String
 
 
 # Test ideas
+# - Create new variant with int returns int value
+# - Create new variant with float returns float value
+# - Create new variant with str returns str value
 # TODO Create other variants as needed
 class TestNativeVariantAdapter(unittest.TestCase):
     def test_canary(self):
         assert_that(2 + 2, equal_to(4))
+
+    def test_create_int_variant_returns_int_value(self):
+        sut = nva.create_variant(-79, nva.PythonVariantTypes.INT32)
+
+        assert_that(sut.value, equal_to(-79))
+
+    def test_create_int_variant_returns_variant_with_int_type(self):
+        sut = nva.create_variant(-51, nva.PythonVariantTypes.INT32)
+
+        assert_that(sut.type, equal_to(nva.PythonVariantTypes.INT32))
 
     def test_get_value_of_int_variant_returns_int_in_native_variant(self):
         stub_net_variant = tsn.VariantDto(89, Int32).create_net_stub()
