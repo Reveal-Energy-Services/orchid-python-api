69403856ca66e9834c40868e9e5c3d2af7cb6838
Larry Jones <larry.jones@reveal-energy.com>
2022-03-12 20:02:38 -0600

    Test ideas for stage correction status variant

--- tests/test_native_variant_adapter.py
+++ tests/test_native_variant_adapter.py
@@ -1,16 +1,16 @@
-#  Copyright 2017-2021 Reveal Energy Services, Inc 
+#  Copyright 2017-2021 Reveal Energy Services, Inc
 #
-#  Licensed under the Apache License, Version 2.0 (the "License"); 
-#  you may not use this file except in compliance with the License. 
-#  You may obtain a copy of the License at 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0 
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software 
-#  distributed under the License is distributed on an "AS IS" BASIS, 
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-#  See the License for the specific language governing permissions and 
-#  limitations under the License. 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 #
 # This file is part of Orchid and related technologies.
 #
@@ -29,7 +29,12 @@ from System import Int32, Double, String
 
 
 # Test ideas
+# - Create stage correction status variant returns correct value
+# - Create stage correction status variant returns correct type
+# - Get value of stage correction status variant returns correct value
+# - Type of stage correction status variant returns correct value
 # TODO Create other variants as needed
+
 class TestNativeVariantAdapter(unittest.TestCase):
     def test_canary(self):
         assert_that(2 + 2, equal_to(4))
