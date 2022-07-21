ab8e1577ec6bfda2422fd7c29b111fa6f68280a2
Larry Jones <larry.jones@reveal-energy.com>
2022-07-21 10:49:54 -0500

    Illustrate usage through docstest examples
    Previous commits put these doctest examples in the "lower-level"
    `ProjectStore` class.
    
    This commit illustrates a developer using these methods from
    `orchid.core`

--- orchid/core.py
+++ orchid/core.py
@@ -23,6 +23,9 @@ import deal
 from orchid.project import Project
 from orchid.project_store import ProjectStore
 
+# To support doctests only
+import orchid
+
 
 @deal.pre(lambda ifrac_pathname: len(ifrac_pathname.strip()) != 0)
 @deal.pre(lambda ifrac_pathname: ifrac_pathname is not None)
@@ -35,6 +38,12 @@ def load_project(ifrac_pathname: str) -> Project:
 
     Returns:
         The project of interest.
+
+    Examples:
+        >>> load_path = orchid.training_data_path().joinpath('frankNstein_Bakken_UTM13_FEET.ifrac')
+        >>> loaded_project = orchid.load_project(str(load_path))
+        >>> loaded_project.name
+        'frankNstein_Bakken_UTM13_FEET'
     """
     loader = ProjectStore(ifrac_pathname.strip())
     result = Project(loader)
@@ -52,6 +61,15 @@ def save_project(project: Project, ifrac_pathname: str) -> None:
     Args:
         ifrac_pathname: The path identifying the data file of the project of interest.
         project: The project of interest.
+
+    Examples:
+        >>> # Test saving changed project
+        >>> load_path = orchid.training_data_path().joinpath('frankNstein_Bakken_UTM13_FEET.ifrac')
+        >>> loaded_project = orchid.load_project(str(load_path))
+        >>> save_path = load_path.with_name(f'salvus{load_path.suffix}')
+        >>> orchid.save_project(loaded_project, str(save_path))
+        >>> save_path.exists()
+        True
     """
 
     store = ProjectStore(ifrac_pathname.strip())
