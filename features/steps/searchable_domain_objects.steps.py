013514e9f8e2a52a036868d4fe3683d6f88db71a
laj <larry.jones@reveal-energy.com>
2022-11-02 16:02:02 -0500

    Reduce common test code

--- features/steps/searchable_domain_objects.steps.py
+++ features/steps/searchable_domain_objects.steps.py
@@ -31,6 +31,11 @@ def step_impl(context, name, surrounding):
         name (str): The name of the data frame.
         surrounding (str): The kind of whitespace surrounding the data frame name in the query.
     """
+    context.queried_domain_objects = _query_project_objects_by_find_func(context, name, surrounding,
+                                                                         lambda src, qn: list(src.find_by_name(qn)))
+
+
+def _query_project_objects_by_find_func(context, name, surrounding, find_by_func):
     if surrounding == 'no':
         query_name = name
     elif surrounding == 'leading':
@@ -52,7 +57,7 @@ def step_impl(context, name, surrounding):
     with warnings.catch_warnings(record=False):
         if context.project.name == 'PermianProjectQ3_2022':
             warnings.simplefilter("ignore")
-        context.queried_domain_objects = list(context.project.data_frames().find_by_name(query_name))
+        return find_by_func(context.project.data_frames(), query_name)
 
 
 @then("I see a single data frame named '{name}' with no surrounding whitespace")
@@ -75,28 +80,8 @@ def step_impl(context, display_name, surrounding):
         display_name (str): The display name of the data frame.
         surrounding (str): The kind of whitespace surrounding the data frame name in the query.
     """
-    if surrounding == 'no':
-        query_name = display_name
-    elif surrounding == 'leading':
-        query_name = ' ' * random.choice(range(1, 3 + 1)) + display_name
-    elif surrounding == 'trailing':
-        query_name = display_name + ' ' * random.choice(range(1, 3 + 1))
-    elif surrounding == 'leading and trailing':
-        query_name = ' ' * random.choice(range(1, 3 + 1)) + display_name + ' ' * random.choice(range(1, 3 + 1))
-    else:
-        raise ValueError(f'Unexpected surrounding text: "{surrounding}"')
-
-    # TODO: Remove catching warnings if we change the integration test data file,
-    #  "c:\src\Orchid.IntegrationTestData\05PermianProjectQ3_2022_DataFrames.ifrac"
-    #
-    # I currently ignore these warnings only for this single project because it is the only project in the
-    # integration test data that has duplicate object IDs in data frames. I ignore it because I do not want printing
-    # the warning to act as a "false positive" for a developer investigating another issue, seeing this expected
-    # warning and wondering (or investigating) the issue.
-    with warnings.catch_warnings(record=False):
-        if context.project.name == 'PermianProjectQ3_2022':
-            warnings.simplefilter("ignore")
-        context.queried_domain_objects = list(context.project.data_frames().find_by_display_name(query_name))
+    context.queried_domain_objects = _query_project_objects_by_find_func(
+        context, display_name, surrounding, lambda src, qn: list(src.find_by_display_name(qn)))
 
 
 @then("I see a single data frame with display name '{display_name}' and no surrounding whitespace")
